cca579d492472dd50d875c452ad20c1b2d4fe236
Bob Weichler <bobmasaki@hotmail.com>
2018-12-05 11:03:30 -0500

    Initialize scroll function

--- progress-scroll.js
+++ progress-scroll.js
@@ -1,7 +1,18 @@
 $(function(){
 	console.log('loaded.');
+	progress_scroll({
+		element: $('#progress-scroll')
+	});
 });
 
-$(document).on('scroll', function(){
+progress_scroll = ({element = ''}) => {
+	window.addEventListener('scroll', function(e){
+		let fromTop = window.scrollY | 0;
+		let fromBottom = window.scrollY + window.innerHeight | 0;
 
-});
+		if(element.length){
+			console.log('found!');
+			// console.log('found');
+		}
+	});
+}
